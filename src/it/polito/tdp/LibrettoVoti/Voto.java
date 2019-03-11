7f1525a992e6008eaf79f95611e9cd839519137c
Paolo FIorio <Paolo FIorio@172.22.57.19>
2019-03-11 14:03:42 +0100

    Classe libretto vuota

--- src/it/polito/tdp/LibrettoVoti/Voto.java
+++ src/it/polito/tdp/LibrettoVoti/Voto.java
@@ -4,6 +4,24 @@ import java.time.LocalDate;
 
 public class Voto {
 	private int voto;
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
 	private String corso;
 	private LocalDate data;
 	
