7159de2ccb6ec01d3a2fb4fe24e56fce608f7932
Paolo FIorio <Paolo FIorio@172.22.57.19>
2019-03-11 13:52:45 +0100

    Aggiunti metodi standard

--- src/it/polito/tdp/LibrettoVoti/Voto.java
+++ src/it/polito/tdp/LibrettoVoti/Voto.java
@@ -1,5 +1,11 @@
 package it.polito.tdp.LibrettoVoti;
 
-public class Voto {
+import java.time.LocalDate;
 
+public class Voto {
+	private int voto;
+	private String corso;
+	private LocalDate data;
+	
+	
 }
